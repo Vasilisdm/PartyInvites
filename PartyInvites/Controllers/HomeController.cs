401b62c405d7ef9aadc95d68e47e597c204ec68a
vasilisdimitriou <vasilisdmr@gmail.com>
2019-05-19 21:59:06 +0300

    Changed index method to return MyView

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -7,12 +7,10 @@ namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        /*
         public ViewResult Index()
         {
-
+            return View("MyView");
         }
-        */      
 
         public ViewResult RsvpForm()
         {
